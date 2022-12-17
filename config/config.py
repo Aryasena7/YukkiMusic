cbeb62fb375cab9f380c3939ff3d2b1fd9f6f7f3
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-18 00:21:26 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/muhammadhanif71/yuki-ori",
+    "https://github.com/muhammadhanif71/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
